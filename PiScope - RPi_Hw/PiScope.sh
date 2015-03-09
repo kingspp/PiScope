21691d622f39a22872a00ea887bd658b7cacc406
Prathyush <kingspprathyush@gmail.com>
2015-03-10 00:03:07 +0530

    Create PiScope.sh

---
+++ PiScope - RPi_Hw/PiScope.sh
@@ -0,0 +1,19 @@
+defSleep=0;
+
+echo "Welcome to Automated Run"
+
+echo "Initializing"
+sleep $defSleep
+
+cd /home/pi/Desktop/bcm2835-1.38/examples/spin
+gcc -o spin spin.c -l bcm2835
+sudo ./spin
+wait $!
+
+gnuplot -persist -e "plot \"values.dat\" using 1:2 with lines;"
+
+#gnuplot <<__EOF
+#plot "values.dat" using 1:2 with lines -persist
+#pause -1
+#__EOF
+
